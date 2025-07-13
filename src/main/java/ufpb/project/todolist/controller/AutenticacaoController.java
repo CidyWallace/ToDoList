707d68176d0701b5d4eeb1a2fdd6827ed90f05f3
cidy <cidywallace12@gmail.com>
2025-07-13 16:56:38 -0300

    Controller, Service, DAO de autenticação criados

---
+++ src/main/java/ufpb/project/todolist/controller/AutenticacaoController.java
@@ -0,0 +1,36 @@
+package ufpb.project.todolist.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ufpb.project.todolist.domain.usuario.DadosAutenticacao;
+import ufpb.project.todolist.domain.usuario.Usuario;
+import ufpb.project.todolist.infra.security.DadosTokenJWT;
+import ufpb.project.todolist.infra.security.TokenService;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    private final TokenService tokenService;
+    private final AuthenticationManager manager;
+
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService) {
+        this.manager = manager;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping
+    public ResponseEntity<DadosTokenJWT> autenticar(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authenticacion = manager.authenticate(authenticationToken);
+
+        var token = tokenService.gerarToken((Usuario) authenticacion.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(token));
+    }
+}
---
+++ src/main/java/ufpb/project/todolist/service/AutenticacaoService.java
@@ -0,0 +1,24 @@
+package ufpb.project.todolist.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ufpb.project.todolist.infra.security.TokenService;
+import ufpb.project.todolist.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
